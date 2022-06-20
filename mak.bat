6c9437ba5c6849a6725d65062fa6908bcce34733
neige <neige@neige.nu>
2022-06-20 23:11:56 +0900

    mak.bat build ディレクトリと cmake オプションを変数化
    mak.bat build ディレクトリと cmake オプションを変数 @build, @cmake_opt
    に代入し、コードの共通部分を増やした

--- mak.bat
+++ mak.bat
@@ -1,6 +1,6 @@
 @rem <mak.bat> -*- coding: cp932-dos -*-
 @echo off
-rem Time-stamp: <2022-06-20 23:01:02 neige>
+rem Time-stamp: <2022-06-20 23:07:48 neige>
 rem
 rem Project try-cpp-winlin
 rem Copyright (C) 2022 neige68
@@ -31,10 +31,13 @@ if "%VCToolsVersion%"=="" call "%VC%\vcvarsall.bat" x86
 
 rem ------------------------------------------------------------
 rem build
-if not exist build mkdir build
-pushd build
+set @build=build
+set @cmake_opt=-A Win32
+
+if not exist %@build% mkdir %@build%
+pushd %@build%
 if not exist ALL_BUILD.vcxproj set @exec_cmake=t
-if not "%@exec_cmake%"=="" cmake -A Win32 ..
+if not "%@exec_cmake%"=="" cmake %@cmake_opt% ..
 msbuild ALL_BUILD.vcxproj /p:Configuration=Debug /m
 echo INFO: mak.bat: msbuild Debug Done.
 if errorlevel 1 goto err_pop2
@@ -45,10 +48,13 @@ popd
 
 rem ------------------------------------------------------------
 rem buildu
-if not exist buildu mkdir buildu
-pushd buildu
+set @build=buildu
+set @cmake_opt=-A Win32 -D UNICODE=1
+
+if not exist %@build% mkdir %@build%
+pushd %@build%
 if not exist ALL_BUILD.vcxproj set @exec_cmake=t
-if not "%@exec_cmake%"=="" cmake -A Win32 -D UNICODE=1 ..
+if not "%@exec_cmake%"=="" cmake %@cmake_opt% ..
 msbuild ALL_BUILD.vcxproj /p:Configuration=Debug /m
 echo INFO: mak.bat: msbuild Debug Done.
 if errorlevel 1 goto err_pop2
@@ -59,10 +65,13 @@ popd
 
 rem ------------------------------------------------------------
 rem build64
-if not exist build64 mkdir build64
-pushd build64
+set @build=build64
+set @cmake_opt=-A x64
+
+if not exist %@build% mkdir %@build%
+pushd %@build%
 if not exist ALL_BUILD.vcxproj set @exec_cmake=t
-if not "%@exec_cmake%"=="" cmake -A x64 ..
+if not "%@exec_cmake%"=="" cmake %@cmake_opt% ..
 msbuild ALL_BUILD.vcxproj /p:Configuration=Debug /m
 echo INFO: mak.bat: msbuild Debug Done.
 if errorlevel 1 goto err_pop2
@@ -73,10 +82,13 @@ popd
 
 rem ------------------------------------------------------------
 rem build64u
-if not exist build64u mkdir build64u
-pushd build64u
+set @build=build64u
+set @cmake_opt=-A x64 -D UNICODE=1
+
+if not exist %@build% mkdir %@build%
+pushd %@build%
 if not exist ALL_BUILD.vcxproj set @exec_cmake=t
-if not "%@exec_cmake%"=="" cmake -A x64 -D UNICODE=1 ..
+if not "%@exec_cmake%"=="" cmake %@cmake_opt% ..
 msbuild ALL_BUILD.vcxproj /p:Configuration=Debug /m
 echo INFO: mak.bat: msbuild Debug Done.
 if errorlevel 1 goto err_pop2
